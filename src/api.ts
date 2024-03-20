73802a4bc8362a7e6698d9fa19caa3ea723a365e
xinyu <rizzlecheng@gmail.com>
2024-03-20 12:05:31 +0100

    fix: mock api

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { DataInterface } from "./types";
 // mock api
-axios.defaults.baseURL = "http://localhost:3000";
 
 export async function getAudio() {
   const res = await axios.get("/test_audio_files.json");
